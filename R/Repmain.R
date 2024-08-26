45779f30aa392b32532c97456ea6a932f75410c1
ninhtran02 <134125488+ninhtran02@users.noreply.github.com>
2024-08-27 09:27:55 +1000

    Typo fix
    Fixed Typo in parfilter function

--- R/Repmain.R
+++ R/Repmain.R
@@ -21,7 +21,7 @@ library("partitions", lib.loc = "~/R/lib")
 library("adaFilter")
 
 source("auto.R")
-source("RepFilterIndependence.R")  
+source("RepFilterIndependence.R")
 source("stirlingfunctions.R")
 source("simulation_functions.R")
 
@@ -46,7 +46,7 @@ methods <- c("PF", "MaxPF", "APF", "MaxAPF", "AutoAPF", "BH-Simes",
 
 if(rho != 0){
   methods <- c(methods, "BY-Simes", "BY-Stouffer", "BY-Fisher", "ad-PF", "prds-PF" )
-} 
+}
 
 
 
@@ -66,7 +66,7 @@ group_options <- allStirlingPartitions(n = n, u = u)[-1]
 #group_options <- SafePartitions(n = n, u = u)
 #if(n == 8){
 #  group_options <- group_options[which(unlist(lapply(group_options, length)) == 2)]
-#} 
+#}
 
 FDRrep <- rep(0,length(methods))
 names(FDRrep) <- methods
@@ -90,10 +90,10 @@ for(iter in 1:nsim){
   data_list <- generate_pvalues(m = m, n = n, u = u, mu = mu, rho = rho)
   p <- data_list$p
   class(p) <- "matrix"
-  
+
   for(method in methods){
     if(method == "PF"){
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                       u = u, groups = NULL, group_options = group_options,
                       selections = NULL, u_groups = NULL,
                       adaptive = FALSE, lambda = NULL,
@@ -102,9 +102,9 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "MaxPF"){
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                        u = u, groups = NULL, group_options = group_options,
                        selections = NULL, u_groups = NULL,
                        adaptive = FALSE, lambda = NULL,
@@ -115,7 +115,7 @@ for(iter in 1:nsim){
     }
 
     if(method == "APF"){
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                        u = u, groups = NULL, group_options = group_options,
                        selections = NULL, u_groups = NULL,
                        adaptive = TRUE, lambda = NULL,
@@ -124,9 +124,9 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "MaxAPF"){
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                        u = u, groups = NULL, group_options = group_options,
                        selections = NULL, u_groups = NULL,
                        adaptive = TRUE, lambda = NULL,
@@ -135,7 +135,7 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "AutoAPF"){
       #if(length(group_options) > 100){
       #  sample_groups <- sample(1:length(group_options),
@@ -144,13 +144,13 @@ for(iter in 1:nsim){
       #  sample_groups <- 1:length(group_options)
       #}
       groups <- group_options[sample(x = length(group_options), size = 1)][[1]]
-      #obj <- parfilterp = p, error_targets = rep(alpha, n + 1),
+      #obj <- parfilter(p = p, error_targets = rep(alpha, n + 1),
       #                 u = u, groups = NULL, group_options = group_options[sample_groups],
       #                 selections = NULL, u_groups = NULL,
       #                 adaptive = TRUE, lambda = NULL,
       #                 auto = TRUE, omega = 0.75, w = NULL,
       #                 partition = NULL, method = "Stouffer")
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                        u = u, groups = groups, group_options = group_options,
                        selections = NULL, u_groups = NULL,
                        adaptive = TRUE, lambda = NULL,
@@ -159,7 +159,7 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BH-Simes"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -170,7 +170,7 @@ for(iter in 1:nsim){
                                                      method = "BH") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BH-Stouffer"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -181,7 +181,7 @@ for(iter in 1:nsim){
                                                      method = "BH") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BH-Fisher"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -192,14 +192,14 @@ for(iter in 1:nsim){
                                                      method = "BH") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "AdaFilter"){
       obj <- adaFilter(p.matrix = p, r = u, type.I.err = "FDR",
                        alpha = alpha/sum(1/1:m))
       rep_rejection_list[[method]] <- which(obj$decision == 1)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BY-Simes"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -210,7 +210,7 @@ for(iter in 1:nsim){
                                                      method = "BY") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BY-Stouffer"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -221,7 +221,7 @@ for(iter in 1:nsim){
                                                      method = "BY") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "BY-Fisher"){
       p_list <- split(c(t(p)), rep(1:nrow(p), each = ncol(p)))
       u_list <- split(rep(u,m), 1:m)
@@ -232,9 +232,9 @@ for(iter in 1:nsim){
                                                      method = "BY") <= alpha)
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "ad-PF"){
-      obj <- parfilterp = p, error_targets = rep(alpha/sum(1/1:m), n + 1),
+      obj <- parfilter(p = p, error_targets = rep(alpha/sum(1/1:m), n + 1),
                        u = u, groups = NULL, group_options = group_options,
                        selections = NULL, u_groups = NULL,
                        adaptive = FALSE, lambda = NULL,
@@ -243,9 +243,9 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
     if(method == "prds-PF"){
-      obj <- parfilterp = p, error_targets = c(rep(alpha/5, n),alpha),
+      obj <- parfilter(p = p, error_targets = c(rep(alpha/5, n),alpha),
                        u = u, groups = NULL, group_options = group_options,
                        selections = "PRDS", u_groups = NULL,
                        adaptive = FALSE, lambda = NULL,
@@ -254,33 +254,33 @@ for(iter in 1:nsim){
       rep_rejection_list[[method]] <- obj$Replicability_Rejections
       study_rejection_list[[method]] <- obj$Studywise_Rejections
     }
-    
+
   }
-  
-  fdprep_numeric <- unlist(lapply(X = rep_rejection_list, FUN = compute_FDP, 
+
+  fdprep_numeric <- unlist(lapply(X = rep_rejection_list, FUN = compute_FDP,
          true_nulls = setdiff(1:m,data_list$rep_nonnull_indices)))
 
-  tpprep_numeric <- unlist(lapply(X = rep_rejection_list, FUN = compute_TPP, 
+  tpprep_numeric <- unlist(lapply(X = rep_rejection_list, FUN = compute_TPP,
          false_nulls = data_list$rep_nonnull_indices))
-  
+
   fdrstudy <- matrix(0,nrow = n, ncol = length(methods))
   colnames(fdrstudy) <- methods
   tprstudy <- matrix(0,nrow = n, ncol = length(methods))
   colnames(tprstudy) <- methods
-  
+
   for(method in methods){
     for(j in 1:n){
       fdrstudy[j, method] <- compute_FDP(rejections = study_rejection_list[[method]][[j]],
                                          true_nulls  = setdiff(1:m,data_list$study_nonnull_indices[[j]]))
-      
+
       tprstudy[j, method] <- compute_TPP(rejections = study_rejection_list[[method]][[j]],
                                          false_nulls = data_list$study_nonnull_indices[[j]])
     }
   }
-  
+
   FDRrep <- FDRrep + fdprep_numeric/nsim
   TPRrep <- TPRrep + tpprep_numeric/nsim
-  
+
   FDRstudy <- FDRstudy + fdrstudy/nsim
   TPRstudy <- TPRstudy + tprstudy/nsim
 }
