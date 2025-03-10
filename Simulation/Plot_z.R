854cde2c328c4fc6c057f63db697c687a9fbb33e
ninhtran02 <134125488+ninhtran02@users.noreply.github.com>
2025-03-10 12:36:26 +1100

    Add files via upload

--- Simulation/Plot_z.R
+++ Simulation/Plot_z.R
@@ -2,11 +2,11 @@ setwd("~/Paper Simulations/Covariate-Assisted")
 library(ggplot2)
 library(latex2exp)
 library(ggpubr)
-xcoef_options <- c(0, 1, 1.5)
-mu_options <- c(0.74, 0.76, 0.78, 0.80, 0.82)
+xcoef_options <- c(0, 1.0, 1.5)
+mu_options <- c(2.0, 2.2, 2.4, 2.6, 2.8)
 u_n_options <- list(c(2,2),c(2,3),c(3,3),c(3,4),c(4,4),c(3,5),c(4,5),c(5,5))
-methods <- c("ParFilter", "BH-Stouffer", "Inflated-AdaFilter-BH",
-             "AdaFilter-BH", "CAMT", "AdaPT", "IHW", "Oracle")
+methods <- c("ParFilter", "BH", "Inflated-AdaFilter-BH",
+             "AdaFilter-BH", "CAMT", "AdaPT", "IHW", "Oracle", "repfdr")
 
 FDR_dat <- data.frame()
 TPR_dat <- data.frame()
@@ -15,7 +15,7 @@ for(xcoef in xcoef_options){
     for(u_n in u_n_options){
       u <- u_n[1]
       n <- u_n[2]
-      file.name <- paste("Independence/","mu",mu,"setup",u,n,"xcoef",xcoef,".RD",sep = "")
+      file.name <- paste("Independencez/","mu",mu,"setup",u,n,"xcoef",xcoef,".RD",sep = "")
       load(file = file.name)
       fdr_dat <- cbind.data.frame(reshape2::melt(FDR_list),xcoef,mu,paste(u,n, sep = ""))
       FDR_dat <- rbind.data.frame(FDR_dat,fdr_dat)
@@ -44,8 +44,8 @@ levels(FDR_dat$un) <- c("2 / '['*2*']'",
                         "5 / '['*5*']'")
 
 FDR_dat$Methods <- as.factor(x = FDR_dat$Methods)
-levels(FDR_dat$Methods) <- c("AdaFilter-BH", "AdaPT", "BH", "CAMT",                 
-                             "IHW", "Inflated-AdaFilter-BH", "Oracle", "ParFilter")
+#levels(FDR_dat$Methods) <- c("AdaFilter-BH", "AdaPT", "BH", "CAMT",                 
+#                             "IHW", "Inflated-AdaFilter-BH", "Inflated-ParFilter", "ParFilter")
 
 TPR_dat$xcoef <- as.factor(x = TPR_dat$xcoef)
 levels(TPR_dat$xcoef) <- c("Non-informative~(gamma[1] == 0)",
@@ -63,21 +63,20 @@ levels(TPR_dat$un) <- c("2 / '['*2*']'",
                         "5 / '['*5*']'")
 
 TPR_dat$Methods <- as.factor(x = TPR_dat$Methods)
-levels(TPR_dat$Methods) <- c("AdaFilter-BH", "AdaPT", "BH", "CAMT",                 
-                             "IHW", "Inflated-AdaFilter-BH", "Oracle", "ParFilter")
+#levels(TPR_dat$Methods) <- c("AdaFilter-BH", "AdaPT", "BH", "CAMT",                 
+#                             "IHW", "Inflated-AdaFilter-BH", "Inflated-ParFilter", "ParFilter")
 
-rev(10:(10-1+length(methods)))
-color_ref <- c(17, 11, 15, 14, 13, 12, 16, 10)
-  
+color_ref <- c(17, 11, 15, 14, 13, 12, 16, 10, 19)
+
 FDR_plot <- ggplot(data=FDR_dat[FDR_dat$un %in% c("2 / '['*2*']'",
                                                   "3 / '['*3*']'", 
                                                   "4 / '['*4*']'",
                                                   "5 / '['*5*']'"),], aes(x=mu,
-                                     y = FDR,
-                                     group = Methods,
-                                     linetype=Methods,
-                                     shape=Methods,
-                                     color=Methods)) +
+                                                                                  y = FDR,
+                                                                                  group = Methods,
+                                                                                  linetype=Methods,
+                                                                                  shape=Methods,
+                                                                                  color=Methods)) +
   geom_line() +
   geom_point() +
   scale_shape_manual(values=seq(0,length(methods))) + 
@@ -85,7 +84,7 @@ FDR_plot <- ggplot(data=FDR_dat[FDR_dat$un %in% c("2 / '['*2*']'",
   facet_grid(un ~ xcoef, labeller = label_parsed) +
   #scale_y_continuous(breaks = c(0,0.025,0.05,0.075,0.10)) +
   ylab(TeX(r'(FDR$_{rep}$)')) +
-  xlab( TeX(r'($\xi$)') ) +
+  xlab( TeX(r'($\mu$)') ) +
   theme_bw() +
   geom_hline(yintercept = 0.05, linetype='dotted', size = 1) +
   theme(legend.title=element_blank())
@@ -94,11 +93,11 @@ TPR_plot <- ggplot(data=TPR_dat[TPR_dat$un %in% c("2 / '['*2*']'",
                                                   "3 / '['*3*']'", 
                                                   "4 / '['*4*']'",
                                                   "5 / '['*5*']'"),], aes(x=mu,
-                                     y = TPR,
-                                     group = Methods,
-                                     linetype=Methods,
-                                     shape=Methods,
-                                     color=Methods)) +
+                                                                                  y = TPR,
+                                                                                  group = Methods,
+                                                                                  linetype=Methods,
+                                                                                  shape=Methods,
+                                                                                  color=Methods)) +
   geom_line() +
   geom_point() +
   scale_shape_manual(values=seq(0,length(methods))) + 
@@ -106,13 +105,13 @@ TPR_plot <- ggplot(data=TPR_dat[TPR_dat$un %in% c("2 / '['*2*']'",
   facet_grid(un ~ xcoef, labeller = label_parsed) +
   #scale_y_continuous(breaks = c(0,0.025,0.05,0.075,0.10)) +
   ylab(TeX(r'(TPR$_{rep}$)')) +
-  xlab( TeX(r'($\xi$)') ) +
+  xlab( TeX(r'($\mu$)') ) +
   theme_bw() +
   #geom_hline(yintercept = 0.05, linetype='dotted', size = 1) +
   theme(legend.title=element_blank())
 
-pdf(file = "u = n Results.pdf",
-    width = 9.6, height = 9.6)#12.8)
+pdf(file = "z u = n Results.pdf",
+    width = 9.6, height = 9.6)
 ggarrange(FDR_plot,TPR_plot,ncol = 1, nrow = 2,
           common.legend = TRUE, legend = "bottom")
 dev.off()
@@ -129,7 +128,7 @@ FDR_plot <- ggplot(data=FDR_dat[FDR_dat$un %in% c("2 / '['*3*']'",
   geom_line() +
   geom_point() +
   scale_shape_manual(values=seq(0,length(methods))) + 
-  scale_color_manual(values = color_ref ) +
+  scale_color_manual(values = 10:(10+length(methods)) ) +
   facet_grid(un ~ xcoef, labeller = label_parsed) +
   #scale_y_continuous(breaks = c(0,0.025,0.05,0.075,0.10)) +
   ylab(TeX(r'(FDR$_{rep}$)')) +
@@ -150,7 +149,7 @@ TPR_plot <- ggplot(data=TPR_dat[TPR_dat$un %in% c("2 / '['*3*']'",
   geom_line() +
   geom_point() +
   scale_shape_manual(values=seq(0,length(methods))) + 
-  scale_color_manual(values = color_ref ) +
+  scale_color_manual(values = 10:(10+length(methods)) ) +
   facet_grid(un ~ xcoef, labeller = label_parsed) +
   #scale_y_continuous(breaks = c(0,0.025,0.05,0.075,0.10)) +
   ylab(TeX(r'(TPR$_{rep}$)')) +
@@ -159,7 +158,7 @@ TPR_plot <- ggplot(data=TPR_dat[TPR_dat$un %in% c("2 / '['*3*']'",
   #geom_hline(yintercept = 0.05, linetype='dotted', size = 1) +
   theme(legend.title=element_blank())
 
-pdf(file = "u < n Results.pdf",
+pdf(file = "z u < n Results.pdf",
     width = 9.6, height = 9.6)
 ggarrange(FDR_plot,TPR_plot,ncol = 1, nrow = 2,
           common.legend = TRUE, legend = "bottom")
