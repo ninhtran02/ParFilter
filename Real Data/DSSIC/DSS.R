56ec4a29a9e8c9d480c2c18c9b6974acfc15bd53
ninhtran02 <134125488+ninhtran02@users.noreply.github.com>
2026-01-14 12:49:27 +1100

    DSSIC

---
+++ Real Data/DSSIC/DSS.R
@@ -0,0 +1,177 @@
+# Load the data
+#load("GSE34553_data.RData")
+#load("GSE35651_data.RData")
+
+gene_names <- intersect(names(p_GSE34553), names(p_GSE35651))
+gene_names <- gene_names[-which(gene_names == "")]
+m <- length(gene_names)
+p_mat <- matrix(NA, nrow = m, ncol = 2)
+rownames(p_mat) <- gene_names
+p_mat[,1] <- p_GSE34553[gene_names]
+p_mat[,2] <- p_GSE35651[gene_names]
+
+final_covars_GSE34553 <- covars_GSE34553[gene_names]
+final_covars_GSE35651 <- covars_GSE35651[gene_names,]
+final_covars_mat <- cbind(final_covars_GSE34553,final_covars_GSE35651)
+
+X_list <- list(splines::bs(final_covars_GSE34553, df = 6), 
+               cbind(splines::bs(final_covars_GSE35651[,1], df = 6),
+                     splines::bs(final_covars_GSE35651[,2], df = 6)) )
+
+methods <- c("ParFilter", "BH", "Inflated-AdaFilter-BH",
+             "AdaFilter-BH", "CAMT", "AdaPT", "IHW",
+             "CoFilter-BH", "No-Covar-ParFilter")
+
+q <- 0.05
+u <- 2
+K <- 2
+lambda <- 0.5
+R_05_list <- rep(list(0), length(methods))
+names(R_05_list) <- methods
+PF_covars <- do.call(cbind, X_list)
+
+m <- nrow(p_mat)
+
+for(method in methods){
+  
+  if(method == "ParFilter"){
+    R_set <- ParFilter(P = p_mat, u = 2, X_list = list(PF_covars, PF_covars),
+                       K = 2, direction = "negative",
+                       q = 0.05, lambda_vec =  rep(lambda,K))
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "No-Covar-ParFilter"){
+    X_list_no_covar <- list(rep(0,m),rep(0,m))
+    R_set <- ParFilter(P = p_mat, u = 2, X_list = X_list_no_covar,
+                       K = 2, direction = "negative",
+                       q = 0.05, lambda_vec =  rep(lambda,K))
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "Inflated-AdaFilter-BH"){
+    R_set <- AdaFilter_procedure(p_mat = p_mat, u = u, q = q/sum(1/(1:m)))
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "AdaFilter-BH"){
+    R_set <- AdaFilter_procedure(p_mat = p_mat, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "CoFilter-BH"){ 
+    R_set <- CoFilter_procedure(p_mat = p_mat, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "Adaptive-CoFilter-BH"){ 
+    R_set <- Adaptive_CoFilter_procedure(p_mat = p_mat, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "BH"){
+    R_set <- BH_procedure(p_mat = p_mat, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "CAMT"){
+    R_set <- c()
+    R_set <- CAMT_procedure(p_mat = p_mat, u = u, q = q, X_mat = PF_covars)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "AdaPT"){
+    R_set <- AdaPT_procedure(p_mat = p_mat, X_mat = PF_covars, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+  
+  if(method == "IHW"){
+    IHW_covars <- (final_covars_GSE34553 + final_covars_GSE35651[,1] + final_covars_GSE35651[,2])/3
+    R_set <- IHW_procedure(p_mat = p_mat, X_mat = IHW_covars, u = u, q = q)
+    R_05_list[[method]] <- R_set
+  }
+}
+
+methods_index <- c(1,2,5,7,8,9,10,12,15)
+color_ref <- c("#3b444b", 11, # "AdaFilter-BH", "AdaPT"
+               "#FF5722", # "Adaptive-BH"
+               "#e6d7ff", #Adaptive-CoFilter-BH
+               15, # "BH"
+               "#795548", # "BY"
+               14, #"CAMT",
+               "#DE7E5D", # "CoFilter-BH"
+               13, 12, # "IHW", #"Inflated-AdaFilter-BH"
+               "#673AB7", #"Inflated-ParFilter"
+               "#ae0000", # "No-Covar-ParFilter"
+               "#808000", # "Non-adaptive-ParFilter"
+               16, # "Oracle"
+               "#0006b1") # "ParFilter"
+
+alpha_ref <- c(0.70, 0.70, # "AdaFilter-BH", "AdaPT"
+               0.70, # "Adaptive-BH"
+               0.70, # #Adaptive-CoFilter-BH
+               0.70, # "BH"
+               0.70, # "BY"
+               0.70, #"CAMT"
+               0.70, # CoFilter-BH
+               0.70, 0.70, # "IHW", "Inflated-AdaFilter-BH"
+               0.70, #"Inflated-ParFilter"
+               1, # "No-Covar-ParFilter"
+               0.70, # "Non-adaptive-ParFilter"
+               0.70, # "Oracle"
+               1) # "ParFilter"
+
+color_ref <- color_ref[methods_index]
+alpha_ref <- alpha_ref[methods_index]
+
+R_05_dat <- data.frame(Method = names(R_05_list), Rejections = unlist(lapply(R_05_list, length)))
+R_05_dat$Method <- as.factor(R_05_dat$Method)
+# Make the plots
+R_05_plot <- ggplot(R_05_dat, aes(x=Method, y= Rejections, fill=Method )) +  
+  geom_bar(stat = "identity" ) +
+  theme_bw() +
+  scale_fill_manual(values = color_ref ) +
+  scale_alpha_manual(values = alpha_ref) +
+  theme(legend.position = "none", axis.text.x = element_text(color = "Black",
+                                                             size = 8, angle = 45, vjust = 0.5)) +
+  #geom_text(aes(label=Rejections), vjust=1.6, color="white",
+  #          position = position_dodge(0.9), size=3.5)
+  geom_text(aes(label=Rejections), vjust=-0.5, color="black",
+            position = position_dodge(0.9), size=3.5)
+
+pdf(file = "DSS_lambda050.pdf",
+    width = 6, height = 5)
+R_05_plot
+dev.off()
+
+reference_PC_p_values <- apply(X = p_mat, MARGIN = 1, FUN = stouffer_fun, u = 2)
+names(reference_PC_p_values) <- gene_names
+
+not_PF_R_set <- unique(c(R_05_list$BH,
+                       R_05_list$`Inflated-AdaFilter-BH`,
+                       R_05_list$`AdaFilter-BH`,
+                       R_05_list$CAMT,
+                       R_05_list$AdaPT,
+                       R_05_list$IHW,
+                       R_05_list$`CoFilter-BH`,
+                       R_05_list$`No-Covar-ParFilter`))
+PF_R_set <- unique(c(R_05_list$ParFilter))
+  
+length(sort(reference_PC_p_values[setdiff(PF_R_set,not_PF_R_set)]))
+# TOP 15 genes by PF
+cbind(sort(reference_PC_p_values[setdiff(PF_R_set,not_PF_R_set)])[1:15])
+
+
+### PF and No Covar
+not_PF_R_set <- unique(c(R_05_list$BH,
+                         R_05_list$`Inflated-AdaFilter-BH`,
+                         R_05_list$`AdaFilter-BH`,
+                         R_05_list$CAMT,
+                         R_05_list$AdaPT,
+                         R_05_list$IHW,
+                         R_05_list$`CoFilter-BH`))
+PF_R_set <- unique(c(R_05_list$ParFilter,R_05_list$`No-Covar-ParFilter`))
+
+length(sort(reference_PC_p_values[setdiff(PF_R_set,not_PF_R_set)]))
+# TOP 15 genes by PF and No-covar-PF
+cbind(sort(reference_PC_p_values[setdiff(PF_R_set,not_PF_R_set)])[1:15])
---
+++ Real Data/DSSIC/GSE34553_DSS.R
@@ -0,0 +1,56 @@
+# Version info: R 4.2.2, Biobase 2.58.0, GEOquery 2.66.0, limma 3.54.0
+################################################################
+#   Differential expression analysis with limma
+library(GEOquery)
+library(limma)
+library(umap)
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE34553", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL6885", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# group membership for all samples
+gsms <- "2221110022211100"
+sml <- strsplit(gsms, split="")[[1]]
+
+# log2 transformation
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+# assign samples to groups and set up design matrix
+gs <- factor(sml)
+groups <- make.names(c("WT_UT","WT_DSS","DSS_MTA"))
+levels(gs) <- groups
+gset$group <- gs
+design <- model.matrix(~group + 0, gset)
+colnames(design) <- levels(gs)
+
+gset <- gset[complete.cases(exprs(gset)), ] # skip missing values
+
+fit <- lmFit(gset, design)  # fit linear model
+
+# set up contrasts of interest and recalculate model coefficients
+cts <- c(paste(groups[1],"-",groups[2],sep=""))
+cont.matrix <- makeContrasts(contrasts=cts, levels=design)
+cont.matrix <- makeContrasts(
+  WT_UT - WT_DSS, DSS_MTA,
+  levels = colnames(design))
+cont.matrix
+
+fit2 <- contrasts.fit(fit, cont.matrix)
+
+# compute statistics and table of top significant genes
+fit2 <- eBayes(fit2, 0.01)
+p_GSE34553 <- fit2$p.value[,1]
+names(p_GSE34553) <- fit2$genes$Gene.symbol
+covars_GSE34553 <- fit2$t[,2]
+names(covars_GSE34553) <- fit2$genes$Gene.symbol
